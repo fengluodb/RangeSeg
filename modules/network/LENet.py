b8feabdeac839b8dae0297c77567430901261bcf
fengluodb <woaibiancheng31@gmail.com>
2023-04-22 21:28:49 +0800

    feat: release code

---
+++ modules/network/LENet.py
@@ -0,0 +1,297 @@
+import torch.nn as nn
+import torch
+from torch.nn import functional as F
+import numpy as np
+
+def conv3x3(in_planes, out_planes, stride=1, groups=1, dilation=1):
+    """3x3 convolution with padding"""
+    return nn.Conv2d(in_planes, out_planes, kernel_size=3, stride=stride,
+                     padding=dilation, groups=groups, bias=False, dilation=dilation)
+
+def conv1x1(in_planes, out_planes, stride=1):
+    """1x1 convolution"""
+    return nn.Conv2d(in_planes, out_planes, kernel_size=1, stride=stride, bias=False)
+
+
+class BasicConv2d(nn.Module):
+    def __init__(self, in_planes, out_planes, kernel_size, stride=1, padding=0, dilation=1, relu=True):
+        super(BasicConv2d, self).__init__()
+        self.relu = relu
+        self.conv = nn.Conv2d(in_planes, out_planes,
+                              kernel_size=kernel_size, stride=stride,
+                              padding=padding, dilation=dilation, bias=False)
+        self.bn = nn.BatchNorm2d(out_planes)
+        if self.relu:
+            self.relu = nn.LeakyReLU()
+
+    def forward(self, x):
+        x = self.conv(x)
+        x = self.bn(x)
+        if self.relu:
+            x = self.relu(x)
+        return x
+
+class Final_Model(nn.Module):
+
+    def __init__(self, backbone_net, semantic_head):
+        super(Final_Model, self).__init__()
+        self.backend = backbone_net
+        self.semantic_head = semantic_head
+
+    def forward(self, x):
+        middle_feature_maps = self.backend(x)
+
+        semantic_output = self.semantic_head(middle_feature_maps)
+
+        return semantic_output
+
+
+class BasicBlock(nn.Module):
+    expansion = 1
+
+    def __init__(self, inplanes, planes, stride=1, downsample=None, groups=1,
+                 base_width=64, dilation=1, if_BN=None):
+        super(BasicBlock, self).__init__()
+        self.if_BN = if_BN
+        if self.if_BN:
+            norm_layer = nn.BatchNorm2d
+        if groups != 1 or base_width != 64:
+            raise ValueError('BasicBlock only supports groups=1 and base_width=64')
+        if dilation > 1:
+            raise NotImplementedError("Dilation > 1 not supported in BasicBlock")
+        # Both self.conv1 and self.downsample layers downsample the input when stride != 1
+        self.conv1 = conv3x3(inplanes, planes, stride)
+        if self.if_BN:
+            self.bn1 = norm_layer(planes)
+        self.relu = nn.LeakyReLU()
+        self.conv2 = conv3x3(planes, planes)
+        if self.if_BN:
+            self.bn2 = norm_layer(planes)
+        self.downsample = downsample
+        self.stride = stride
+
+    def forward(self, x):
+        identity = x
+
+        out = self.conv1(x)
+        if self.if_BN:
+            out = self.bn1(out)
+        out = self.relu(out)
+
+        out = self.conv2(out)
+        if self.if_BN:
+            out = self.bn2(out)
+        if self.downsample is not None:
+            identity = self.downsample(x)
+        out += identity
+        out = self.relu(out)
+        return out
+
+
+class AttentionModule(nn.Module):
+    def __init__(self, dim):
+        super().__init__()
+        self.conv0 = nn.Conv2d(dim, dim, 5, padding=2, groups=dim)
+
+        self.conv1_1 = nn.Conv2d(dim, dim, (1, 3), padding=(0, 1), groups=dim)
+        self.conv1_2 = nn.Conv2d(dim, dim, (3, 1), padding=(1, 0), groups=dim)
+
+        self.conv2_1 = nn.Conv2d(dim, dim, (1, 5), padding=(0, 2), groups=dim)
+        self.conv2_2 = nn.Conv2d(dim, dim, (5, 1), padding=(2, 0), groups=dim)
+
+        self.conv3_1 = nn.Conv2d(dim, dim, (1, 7), padding=(0, 3), groups=dim)
+        self.conv3_2 = nn.Conv2d(dim, dim, (7, 1), padding=(3, 0), groups=dim)
+
+        self.conv4 = nn.Conv2d(dim, dim, 1)
+
+    def forward(self, x):
+        attn = self.conv0(x)
+
+        attn_0 = self.conv1_1(attn)
+        attn_0 = self.conv1_2(attn_0)
+
+        attn_1 = self.conv2_1(attn)
+        attn_1 = self.conv2_2(attn_1)
+
+        attn_2 = self.conv3_1(attn)
+        attn_2 = self.conv3_2(attn_2)
+
+        attn = attn + attn_0 + attn_1 + attn_2
+
+        attn = self.conv4(attn)
+
+        return attn * x
+
+
+class SpatialAttention(nn.Module):
+    def __init__(self, dim):
+        super().__init__()
+        self.proj_1 = nn.Conv2d(dim, dim, 1)
+        self.act = nn.LeakyReLU()
+        self.spatial_gating_unit = AttentionModule(dim)
+        self.proj_2 = nn.Conv2d(dim, dim, 1)
+
+    def forward(self, x):
+        shortcut = x.clone()
+        x = self.proj_1(x)
+        x = self.act(x)
+        x = self.spatial_gating_unit(x)
+        x = self.proj_2(x)
+        x = x + shortcut
+        return x
+
+
+class MyBasicBlock(nn.Module):
+    expansion = 1
+
+    def __init__(self, inplanes, planes, stride=1, downsample=None, groups=1,
+                 base_width=64, dilation=1, if_BN=None):
+        super(MyBasicBlock, self).__init__()
+        if groups != 1 or base_width != 64:
+            raise ValueError('BasicBlock only supports groups=1 and base_width=64')
+        if dilation > 1:
+            raise NotImplementedError("Dilation > 1 not supported in BasicBlock")
+
+        self.downsample = downsample
+
+        self.conv = BasicConv2d(planes, planes, 3, padding=1)
+
+        self.attn = SpatialAttention(planes)
+        self.dropout = nn.Dropout2d(p=0.2)
+
+    def forward(self, x):
+        if self.downsample is not None:
+            x = self.downsample(x)
+
+        out = self.conv(x) 
+        out = out + self.dropout(self.attn(out))
+
+        out += x
+
+        return out
+
+
+class ResNet_34(nn.Module):
+    def __init__(self, nclasses, params, block=MyBasicBlock, layers=[3, 4, 6, 3], if_BN=True, zero_init_residual=False,
+                 norm_layer=None, groups=1, width_per_group=64):
+        super(ResNet_34, self).__init__()
+        self.nclasses = nclasses
+
+        ### mos modification
+        if params['train']['residual']:
+            self.input_size = 5 + params['train']['n_input_scans']
+        else:
+            self.input_size = 5 
+
+        print("Depth of backbone input = ", self.input_size)
+        ###
+
+        if norm_layer is None:
+            norm_layer = nn.BatchNorm2d
+        self._norm_layer = norm_layer
+        self.if_BN = if_BN
+        self.dilation = 1
+        self.aux = params["train"]["aux_loss"]["use"]
+
+        self.groups = groups
+        self.base_width = width_per_group
+
+        self.conv1 = BasicConv2d(5, 64, kernel_size=3, padding=1)
+        self.conv2 = BasicConv2d(64, 128, kernel_size=3, padding=1)
+        self.conv3 = BasicConv2d(128, 128, kernel_size=3, padding=1)
+
+        self.inplanes = 128
+
+        self.layer1 = self._make_layer(block, 128, layers[0])
+        self.layer2 = self._make_layer(block, 128, layers[1], stride=2)
+        self.layer3 = self._make_layer(block, 128, layers[2], stride=2)
+        self.layer4 = self._make_layer(block, 128, layers[3], stride=2)
+
+        self.decoder1 = BasicConv2d(256, 128, 3, padding=1)
+        self.decoder2 = BasicConv2d(256, 128, 3, padding=1)
+        self.decoder3 = BasicConv2d(256, 128, 3, padding=1)
+        self.decoder4 = BasicConv2d(256, 128, 3, padding=1)
+
+        self.fusion_conv = BasicConv2d(128 * 3, 128, kernel_size=1)
+        self.semantic_output = nn.Conv2d(128, nclasses, 1)
+
+        if self.aux:
+            self.aux_head1 = nn.Conv2d(128, nclasses, 1)
+            self.aux_head2 = nn.Conv2d(128, nclasses, 1)
+            self.aux_head3 = nn.Conv2d(128, nclasses, 1)
+
+    def _make_layer(self, block, planes, blocks, stride=1, dilate=False):
+        norm_layer = self._norm_layer
+        downsample = None
+        previous_dilation = self.dilation
+        if dilate:
+            self.dilation *= stride
+            stride = 1
+        if stride != 1 or self.inplanes != planes * block.expansion:
+            if self.if_BN:
+                downsample = nn.Sequential(
+                    # conv1x1(self.inplanes, planes * block.expansion, stride),
+                    nn.AvgPool2d(kernel_size=(3, 3), stride=2, padding=1),
+                    # SoftPool2d(kernel_size=(2, 2), stride=(2, 2)),
+                    norm_layer(planes * block.expansion),
+                )
+            else:
+                downsample = nn.Sequential(
+                    # conv1x1(self.inplanes, planes * block.expansion, stride)
+                    # SoftPool2d(kernel_size=(2, 2), stride=(2, 2))
+                    # nn.AvgPool2d(kernel_size=(3, 3), stride=2, padding=1),
+                )
+
+        layers = []
+        layers.append(block(self.inplanes, planes, stride, downsample, self.groups,
+                            self.base_width, previous_dilation, if_BN=self.if_BN))
+        self.inplanes = planes * block.expansion
+        for _ in range(1, blocks):
+            layers.append(block(planes, planes, groups=self.groups,
+                                base_width=self.base_width, dilation=self.dilation,
+                                if_BN=self.if_BN))
+
+        return nn.Sequential(*layers)
+
+    def forward(self, x):
+
+        x = self.conv1(x)
+        x = self.conv2(x)
+        x = self.conv3(x)
+
+        x_1 = self.layer1(x)  # 1
+        x_2 = self.layer2(x_1)  # 1/2
+        x_3 = self.layer3(x_2)  # 1/4
+        x_4 = self.layer4(x_3)  # 1/8
+
+        res_1 = self.decoder1(torch.cat((x, x_1), dim=1))
+
+        res_2 = F.interpolate(x_2, size=x.size()[2:], mode='bilinear', align_corners=True)
+        res_2 = self.decoder2(torch.cat((res_1, res_2), dim=1))
+
+        res_3 = F.interpolate(x_3, size=x.size()[2:], mode='bilinear', align_corners=True)
+        res_3 = self.decoder3(torch.cat((res_2, res_3), dim=1))
+
+        res_4 = F.interpolate(x_4, size=x.size()[2:], mode='bilinear', align_corners=True)
+        res_4 = self.decoder4(torch.cat((res_3, res_4), dim=1))
+        res = [res_2, res_3, res_4]
+
+        out = torch.cat(res, dim=1)
+        out = self.fusion_conv(out)
+        out = self.semantic_output(out)
+        logits = F.softmax(out, dim=1)
+
+        if self.aux:
+            res_2 = self.aux_head1(res_2)
+            res_2 = F.softmax(res_2, dim=1)
+
+            res_3 = self.aux_head2(res_3)
+            res_3 = F.softmax(res_3, dim=1)
+
+            res_4 = self.aux_head3(res_4)
+            res_4 = F.softmax(res_4, dim=1)
+
+        if self.aux:
+            return [logits, res_2, res_3, res_4]
+        else:
+            return logits, out
