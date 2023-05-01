09af11dde239ff6daeedd7edc2a43d979510e70a
fengluodb <woaibiancheng31@gmail.com>
2023-05-01 20:33:43 +0800

    chore: delete redudant code

--- modules/network/LENet.py
+++ modules/network/LENet.py
@@ -34,50 +34,6 @@ class BasicConv2d(nn.Module):
         return x
 
 
-class BasicBlock(nn.Module):
-    expansion = 1
-
-    def __init__(self, inplanes, planes, stride=1, downsample=None, groups=1,
-                 base_width=64, dilation=1, if_BN=None):
-        super(BasicBlock, self).__init__()
-        self.if_BN = if_BN
-        if self.if_BN:
-            norm_layer = nn.BatchNorm2d
-        if groups != 1 or base_width != 64:
-            raise ValueError(
-                'BasicBlock only supports groups=1 and base_width=64')
-        if dilation > 1:
-            raise NotImplementedError(
-                "Dilation > 1 not supported in BasicBlock")
-        # Both self.conv1 and self.downsample layers downsample the input when stride != 1
-        self.conv1 = conv3x3(inplanes, planes, stride)
-        if self.if_BN:
-            self.bn1 = norm_layer(planes)
-        self.relu = nn.LeakyReLU()
-        self.conv2 = conv3x3(planes, planes)
-        if self.if_BN:
-            self.bn2 = norm_layer(planes)
-        self.downsample = downsample
-        self.stride = stride
-
-    def forward(self, x):
-        identity = x
-
-        out = self.conv1(x)
-        if self.if_BN:
-            out = self.bn1(out)
-        out = self.relu(out)
-
-        out = self.conv2(out)
-        if self.if_BN:
-            out = self.bn2(out)
-        if self.downsample is not None:
-            identity = self.downsample(x)
-        out += identity
-        out = self.relu(out)
-        return out
-
-
 class AttentionModule(nn.Module):
     def __init__(self, dim):
         super().__init__()
